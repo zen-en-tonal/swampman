6732320a4721005ec1c29da4ce97572ed15e4ea1
Takeru KODAMA <140989096+zen-en-tonal@users.noreply.github.com>
2025-10-13 00:47:10 +0900

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule ElixirTemplate.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :my_app,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false},
+      {:telemetry, "~> 1.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
